4e6d22db3bdd144edaae9585be4e90da96cdbcd2
john7325 <john7325@uw.edu>
2022-02-18 14:56:11 -0800

    add chart 2 to index file

--- source/q2_chart.R
+++ source/q2_chart.R
@@ -1,27 +1,31 @@
-temp <- broadband_student_scores %>%
-        filter(StudentGroupType == "All") %>%
-        group_by(GradeLevel,
-                 County,
-                 percent_has_1_medium, 
-                 TestSubject) %>%
-        summarize(avg_met_standard = mean(PercentMetTestedOnly)) 
+filter_data <- function(data) {
+  temp <- broadband_student_scores %>%
+    filter(StudentGroupType == "All") %>%
+    group_by(GradeLevel,
+             County,
+             percent_has_1_medium,
+             TestSubject) %>%
+    summarize(avg_met_standard = mean(PercentMetTestedOnly))
   
-temp$percent_has_1_medium <- temp$percent_has_1_medium %>%
-                             round(2) * 100
+  temp$percent_has_1_medium <- temp$percent_has_1_medium %>%
+    round(2) * 100
+  
+  temp$avg_met_standard <- temp$avg_met_standard * 100
+  temp
+}
 
-temp$avg_met_standard <- temp$avg_met_standard * 100
-
-
-
-ggplot(temp, aes(x = percent_has_1_medium, 
-                   y=avg_met_standard, 
+generate_plot <- function(data) {
+  ggplot(data, aes(x = percent_has_1_medium,
+                   y = avg_met_standard,
                    shape = TestSubject,
-                   color = TestSubject)) + 
-geom_point() + 
-geom_smooth(method = lm, se = FALSE, fullrange = TRUE) + 
-labs(title = "Effects of Broadband Access On Test Subject Performance", 
-     x = "Percent of Students With Access to Broadband",
-     y = "Percent of Students That Met Testing Standards")
+                   color = TestSubject)) +
+  geom_point() +
+  geom_smooth(method = lm, se = FALSE, fullrange = TRUE) +
+  labs(title = "Effects of Broadband Access On Test Subject Performance",
+       x = "Percent of Students With Access to Broadband",
+       y = "Percent of Students That Met Testing Standards")
+}
+
 
 
 
