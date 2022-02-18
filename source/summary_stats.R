831af061ab9f67f0ad1be73b6403aebf4655b9bd
DanielLee <chrisbeneki@gmail.com>
2022-02-17 23:25:41 -0800

    Add summary stats

---
+++ source/summary_stats.R
@@ -0,0 +1,44 @@
+
+# Aggregate Table based on county
+
+
+# 1) lowest Average Score
+
+# average score of counties with medium_has_1 less than 0.5
+
+# 2) Fastest Average Score
+
+# average score of counties with medium_has_1 more than 0.5 
+
+# 3) Doesn't have access to more than 1 Internet speed?   Average Score
+
+# average score of percent_has_more_1_fast
+
+# 4) Access to more than 1 Internet Speed?
+
+# average score of percent_has_more_2_fast
+
+# 5) Low Income with medium_has_1 more than 0.5
+
+# Low Income with medium_has_1 less than 0.5 
+
+library(tidyverse)
+
+slow_percent_met <- function(data) {
+  data %>% 
+    
+}
+  
+fast_percent_met <- function(data) {
+  
+}
+  
+less_percent_met <- function(data) {
+  
+}
+  
+more_percent_met <- function(data) {
+  
+}
+  
+
