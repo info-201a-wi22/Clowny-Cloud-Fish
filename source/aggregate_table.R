0b522d89455a7a30ed3bfefda1e8622d15768a3f
DanielLee <chrisbeneki@gmail.com>
2022-02-17 23:55:28 -0800

    Arrange Order

--- source/aggregate_table.R
+++ source/aggregate_table.R
@@ -1,5 +1,6 @@
 county_aggregate_table <- function(data) {
   data %>% 
     group_by(County, percent_has_1_slow, percent_has_2_slow, percent_has_1_medium, percent_has_2_medium, percent_has_1_fast, percent_has_2_fast) %>% 
-    summarise(PercentMetTestedOnly = mean(PercentMetTestedOnly, na.rm = T))
+    summarise(PercentMetTestedOnly = mean(PercentMetTestedOnly, na.rm = T)) %>% 
+    arrange(PercentMetTestedOnly)
 }
