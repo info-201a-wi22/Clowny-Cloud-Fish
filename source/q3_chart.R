ad6f2bc4b904f18852a2f29357d66d71b027368d
hlee107 <hanna.j.lee@gmail.com>
2022-02-18 19:47:39 -0800

    Edit q3 chart

--- source/q3_chart.R
+++ source/q3_chart.R
@@ -19,140 +19,89 @@ student_broadband_combine <- function(data_2) {
   left_join(broadband, by = c("County" = 'county'))
 }
 
-middle_student_broadband_25 <- student_broadband_combine %>%
+middle_student_broadband_25 <- function(data_3) {
+  ms_data <- data_2 %>%
   filter(speed == 25) %>%
   filter(GradeLevel == "6th Grade" | GradeLevel == "7th Grade" | 
-         GradeLevel == "8th Grade") %>%
-  
+         GradeLevel == "8th Grade")
+}
 
-high_student_broadband_25 <- student_broadband_combine %>%
+high_student_broadband_25 <- function(data_4) {
+  hs_data <- data_2 %>%
   filter(speed == 25) %>%
   filter(GradeLevel == "9th Grade" | GradeLevel == "10th Grade" | 
          GradeLevel == "11th Grade" | GradeLevel == "12th Grade")
+}
 
-order_middle_student_broadband_25 <- within(middle_student_broadband_25, GradeLevel <- 
-                                     factor(GradeLevel, levels = c("6th Grade",
-                                                                   "7th Grade",
-                                                                   "8th Grade")))
+ms_0 <- function(data_3, num_connect) {
+  ggplot(data_3) +
+    geom_boxplot(aes(x = County, y = PercentMetTestedOnly, fill = has_0)) +
+    
+    labs(
+      title = "State Test Satisfactory % vs Grade Level Based on County",
+      subtitle = "(English language learners grades 6 - 8 \n with 0 providers to 25 mbps)",
+      x = "County",
+      y = "% of Who Scored Satisfactory",
+    ) +
+    
+    facet_grid(rows = vars(GradeLevel)) +
+    
+    guides(fill = guide_legend(title="Has 0 \n Broadband \n Providers")) +
+    
+    scale_fill_manual(values = c("#00AFBB")) +
+    
+    theme(axis.text.x = element_text(angle = 90))
+}  
 
-order_high_student_broadband_25 <- within(high_student_broadband_25, GradeLevel <- 
-                                       factor(GradeLevel, levels = c("9th Grade",
-                                                                     "10th Grade",
-                                                                     "11th Grade",
-                                                                     "12th Grade")))
+ms_1 <- function(data_3, num_connect) {
+    ggplot(data_3) +
+    geom_boxplot(aes(x = County, y = PercentMetTestedOnly, fill = has_1)) +
+    
+    labs(
+      subtitle = "(English language learners grades 6 - 8 \n with 1 provider to 25 mbps)",
+      x = "County",
+      y = "% of Who Scored Satisfactory",
+    ) +
+    
+    facet_grid(rows = vars(GradeLevel)) +
+    
+    guides(fill = guide_legend(title="Has 1 \n Broadband \n Provider")) +
+    
+    theme(axis.text.x = element_text(angle = 90))
+}
 
-ggplot(order_student_broadband_25) +
-  geom_boxplot(aes(x = County, y = PercentMetTestedOnly, fill = has_0)) +
-  
-  labs(
-    title = "State Test Satisfactory Percentage vs Grade Level Based on County",
-    subtitle = "(English language learners students grades 6 - 12 with 0 access to 25 mbps)",
-    x = "County",
-    y = "Percentage of Students Who Scored Satisfactory Grades",
-    color = "Broadband Speed (mpbs)"
-  ) +
-  
-  facet_wrap(~GradeLevel) +
-  
-  theme(axis.text.x = element_text(angle = 90))
+hs_0 <- function(data_4) {
+  ggplot(data_4) +
+    geom_boxplot(aes(x = County, y = PercentMetTestedOnly, fill = has_0)) +
+    
+    labs(
+      title = "State Test Satisfactory % vs Grade Level Based on County",
+      subtitle = "(English language learners grades 9 - 12 \n with 0 providers to 25 mbps)",
+      x = "County",
+      y = "",
+    ) +
+    
+    facet_grid(rows = vars(GradeLevel)) +
+    
+    guides(fill = guide_legend(title="Has 0 \n Broadband \n Providers")) +
+    
+    theme(axis.text.x = element_text(angle = 90))
+}
 
-ggplot(order_student_broadband_25) +
-  geom_boxplot(aes(x = County, y = PercentMetTestedOnly, fill = has_1)) +
-  
-  labs(
-    title = "State Test Satisfactory Percentage vs Grade Level Based on County",
-    subtitle = "(English language learners students grades 6 - 12 with 1 access point to 25 mbps)",
-    x = "County",
-    y = "% of Who Scored Satisfactory Grades",
-    color = "Broadband Speed (mpbs)"
-  ) +
-  
-  facet_wrap(~GradeLevel) +
-  
-  guides(fill = guide_legend(title="Has 1 \n Broadband \n Access Point")) +
-  
-  theme(axis.text.x = element_text(angle = 90))
-
-ggplot(order_middle_student_broadband_25) +
-  geom_boxplot(aes(x = County, y = PercentMetTestedOnly, fill = has_0)) +
-  
-  labs(
-    title = "State Test Satisfactory Percentage vs Grade Level Based on County",
-    subtitle = "(English language learners students grades 6 - 12 with 0 access to 25 mbps)",
-    x = "County",
-    y = "% of Who Scored Satisfactory Grades",
-  ) +
-  
-  facet_wrap(~GradeLevel) +
-  
-  guides(fill = guide_legend(title="Has 0 \n Broadband \n Access Point")) +
-  
-  theme(axis.text.x = element_text(angle = 90))
-
-ms_0 <- ggplot(order_middle_student_broadband_25) +
-  geom_boxplot(aes(x = County, y = PercentMetTestedOnly, fill = has_0)) +
-  
-  labs(
-    title = "Middle School State Test Satisfactory Percentage vs Grade Level Based on County",
-    subtitle = "(English language learners grades 6 - 8 \n with 0 access to 25 mbps)",
-    x = "County",
-    y = "% of Who Scored Satisfactory",
-  ) +
-  
-  facet_grid(rows = vars(GradeLevel)) +
-  
-  guides(fill = guide_legend(title="Has 0 \n Broadband \n Access Point")) +
-  
-  scale_fill_manual(values = c("#00AFBB")) +
-  
-  theme(axis.text.x = element_text(angle = 90))
-
-ms_1 <- ggplot(order_middle_student_broadband_25) +
-  geom_boxplot(aes(x = County, y = PercentMetTestedOnly, fill = has_1)) +
-  
-  labs(
-    subtitle = "(English language learners grades 6 - 8 \n with 1 access to 25 mbps)",
-    x = "County",
-    y = "% of Who Scored Satisfactory",
-  ) +
-  
-  facet_grid(rows = vars(GradeLevel)) +
-  
-  guides(fill = guide_legend(title="Has 1 \n Broadband \n Access Point")) +
-  
-  theme(axis.text.x = element_text(angle = 90))
-
-hs_0 <- ggplot(order_high_student_broadband_25) +
-  geom_boxplot(aes(x = County, y = PercentMetTestedOnly, fill = has_0)) +
-  
-  labs(
-    title = "High School State Test Satisfactory Percentage vs Grade Level Based on County",
-    subtitle = "(English language learners grades 9 - 12 \n with 0 access to 25 mbps)",
-    x = "County",
-    y = "",
-  ) +
-  
-  facet_grid(rows = vars(GradeLevel)) +
-  
-  guides(fill = guide_legend(title="Has 0 \n Broadband \n Access Point")) +
-  
-  theme(axis.text.x = element_text(angle = 90))
-
-hs_1 <- ggplot(order_high_student_broadband_25) +
-  geom_boxplot(aes(x = County, y = PercentMetTestedOnly, fill = has_1)) +
-  
-  labs(
-    title = "High School State Test Satisfactory Percentage vs Grade Level Based on County",
-    subtitle = "(English language learners grades 9 - 12 \n with 1 access to 25 mbps)",
-    x = "County",
-    y = "",
-  ) +
-  
-  facet_grid(rows = vars(GradeLevel)) +
-  
-  guides(fill = guide_legend(title="Has 1 \n Broadband \n Access Point")) +
-  
-  theme(axis.text.x = element_text(angle = 90))
-
-ggarrange(ms_0, hs_0, ms_1, hs_1,
-          ncol = 2, nrow =2)
+hs_1 <- function(data_4) {
+  ggplot(data_4) +
+    geom_boxplot(aes(x = County, y = PercentMetTestedOnly, fill = has_1)) +
+    
+    labs(
+      title = "High School State Test Satisfactory Percentage vs Grade Level Based on County",
+      subtitle = "(English language learners grades 9 - 12 \n with 1 provider to 25 mbps)",
+      x = "County",
+      y = "",
+    ) +
+    
+    facet_grid(rows = vars(GradeLevel)) +
+    
+    guides(fill = guide_legend(title="Has 1 \n Broadband \n Provider")) +
+    
+    theme(axis.text.x = element_text(angle = 90))
+}
