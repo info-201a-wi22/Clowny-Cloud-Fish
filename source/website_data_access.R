0bde86e08d508a348d12db7c4e2d320dd9d5734e
nicholas1234 <nicholas.a.vaughn@gmail.com>
2022-03-08 19:56:22 -0800

    fixed website

---
+++ source/website_data_access.R
@@ -0,0 +1,3 @@
+broadband_student_scores <- read.csv("website data/broadband_student_scores.csv")
+test_scores <- read.csv("website data/test_scores.csv")
+broadband <- read.csv("website data/broadband.csv")
