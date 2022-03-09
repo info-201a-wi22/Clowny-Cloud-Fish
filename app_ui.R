a87aa45023d7e68ea22daa82972054c9d9849c9f
nicholas1234 <nicholas.a.vaughn@gmail.com>
2022-03-08 20:35:16 -0800

    added icon

--- app_ui.R
+++ app_ui.R
@@ -10,6 +10,8 @@ source("tabs/report.R")
 ui <- navbarPage(
   tags$i(tags$span(style = "font-family:Lucida Handwriting, Papyrus;color:blue;font-size:30px;font-weight:bold", "Broadband Effect on Education")),
   tags$head(includeCSS("tabs/styles.css")),
+  tags$head(tags$link(rel="shortcut icon", 
+                      href="https://www.aquariumofpacific.org/images/made_new/images-uploads-clownfish_600_q85.jpg")),
   intro_tab,
   chart_1_tab,
   chart_2_tab,
